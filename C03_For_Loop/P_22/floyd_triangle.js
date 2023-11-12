8a07d7e91d9f4b0dcff5f411eaceae7492bc0d37
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-12 15:38:38 +0100

    code fixed

--- C03_For_Loop/P_22/floyd_triangle.js
+++ C03_For_Loop/P_22/floyd_triangle.js
@@ -1,26 +1,20 @@
 // Write a program in C to print Floyd's Triangle.
 
-// 1 
+// 1
 // 01
-// 101 
-// 0101 
+// 101
+// 0101
 // 10101
 
+const trianglefloyd = height => {
 
-const floydTriangle = height => {
-
-for(let i = 1; i <= height; i++){
-  let space = ' '
-  for (let j = 1; j >= 0; j--){
-    space += j + ' ';
+  for (let i = 1; i <= height; i++) {
+    let space = ''
+    for (let j = 1; j <= i; j++) {
+      space += (i + j) % 2 === 0 ? '1' : '0'
+    }
     console.log(space)
   }
-
 }
 
-
-
-
-}
-
-floydTriangle(4)
+trianglefloyd(5)

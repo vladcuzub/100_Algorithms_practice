f91883180e3de763759e924e1f2d76d6522b0bf9
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-21 13:41:18 +0100

    resolved

--- C03_For_Loop/P_43/heighest_factory.js
+++ C03_For_Loop/P_43/heighest_factory.js
@@ -4,3 +4,17 @@
 // Input 2nd number for HCF: 28
 // Expected Output :
 // HCF of 24 and 28 is : 4
+
+const highestCommonFactor = (firstNumber, secondNumber) => {
+  let hcf
+  let smallNumber = firstNumber < secondNumber ? firstNumber : secondNumber
+
+  for (let i = 1; i <= secondNumber; i++) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      hcf = i
+    }
+  }
+  console.log(hcf)
+}
+
+highestCommonFactor(24, 28)

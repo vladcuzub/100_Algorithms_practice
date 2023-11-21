6efc91bac87e62d782af5ac5b005df1108127818
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-21 14:28:47 +0100

    solved

--- C03_For_Loop/P_44/low_number.js
+++ C03_For_Loop/P_44/low_number.js
@@ -4,3 +4,19 @@
 // Input 2nd number for LCM: 20
 // Expected Output :
 // The LCM of 15 and 20 is : 60
+
+const lowCommonMultiple = (firstNumber, secondNumber) => {
+  let hcf
+  let smallNumber = firstNumber < secondNumber ? firstNumber : secondNumber
+
+  for (let i = 1; i <= smallNumber; i++) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      hcf = i
+    }
+  }
+
+  let lcm = (firstNumber * secondNumber) / hcf
+  console.log(lcm)
+}
+
+lowCommonMultiple(15, 20)

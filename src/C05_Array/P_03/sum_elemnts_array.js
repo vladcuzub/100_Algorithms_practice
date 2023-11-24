e5c6cce341a06ced884c0a79a2a667d147f1a121
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-24 22:21:49 +0100

    solved

---
+++ src/C05_Array/P_03/sum_elemnts_array.js
@@ -0,0 +1,37 @@
+// Write a program in C to find the sum of all elements of the array.
+// Test Data :
+// Input the number of elements to be stored in the array :3
+// Input 3 elements in the array :
+// element - 0 : 2
+// element - 1 : 5
+// element - 2 : 8
+// Expected Output :
+// Sum of all elements stored in the array is : 15
+
+
+
+const inputElements = () => {
+    const array = []
+    for (let i = 0; i < 3; i++) {
+        const elements = parseInt(prompt('Add a element: '))
+        array.push(elements)
+    }
+    return array
+}
+
+const calculateElements = (array) => {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum
+}
+
+const main = () => {
+    const ArrayElements = inputElements();
+    const sum = calculateElements(ArrayElements)
+
+    console.log(`Sum of all elements stored in the array is : ${sum}`);
+}
+
+main()

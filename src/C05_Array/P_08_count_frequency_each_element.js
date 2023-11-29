814b8f12be975fa836f10a3763d4d39ef4b3d7f3
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-30 00:00:56 +0100

    add problem

---
+++ src/C05_Array/P_08_count_frequency_each_element.js
@@ -0,0 +1,12 @@
+// Write a program in C to count the frequency of each element of an array.
+// Test Data:
+// Input the number of elements to be stored in the array: 3
+// Input 3 elements in the array:
+// element - 0 : 25
+// element - 1 : 12
+// element - 2 : 43
+// Expected Output:
+// The frequency of all elements of an array:
+// 25 occurs 1 times
+// 12 occurs 1 times
+// 43 occurs 1 times

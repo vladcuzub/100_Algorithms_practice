a1c41e0e43b217dc7e7fc65a695de9ce02aeb4cb
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-12-01 00:00:14 +0100

    add problem

---
+++ src/C05_Array/P_09.js
@@ -0,0 +1,10 @@
+// Write a program in C to find the maximum and minimum elements in an array.
+// Test Data:
+// Input the number of elements to be stored in the array: 3
+// Input 3 elements in the array:
+// element - 0 : 45
+// element - 1 : 25
+// element - 2 : 21
+// Expected Output:
+// Maximum element is: 45
+// Minimum element is: 21

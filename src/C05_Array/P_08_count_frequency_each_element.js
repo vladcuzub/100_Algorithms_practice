985b31b737914795514a697b07e2be15e809608d
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-11-30 23:57:42 +0100

    solved problem with ount the frequency of each element of an array

--- src/C05_Array/P_08_count_frequency_each_element.js
+++ src/C05_Array/P_08_count_frequency_each_element.js
@@ -10,3 +10,32 @@
 // 25 occurs 1 times
 // 12 occurs 1 times
 // 43 occurs 1 times
+
+let array = [];
+
+
+const numbersOfInput = () => {
+    let input = prompt('Input the number of elements to be stored in the array: ')
+
+    for (let i = 0; i < input; i++) {
+        let numb = parseInt(prompt(`Input ${i + 1} element in the array:`));
+        array.push(numb)
+    }
+    return console.log(`Input 3 elements in the array: ${array}`)
+}
+
+const checkFrequnecyElements = () => {
+    for (let i = 0; i < array.length; i++) {
+        let count = 1;
+
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] === array[i]) {
+                count++
+            }
+        }
+        console.log(`${array[i]} occurs ${count} times`);
+    }
+}
+
+numbersOfInput()
+checkFrequnecyElements()

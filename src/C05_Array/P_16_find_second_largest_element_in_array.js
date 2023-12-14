72b0d755117ae32ff5e90313977c8ecfad726282
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-12-14 12:53:05 +0100

    fix logic secondLargestElement

--- src/C05_Array/P_16_find_second_largest_element_in_array.js
+++ src/C05_Array/P_16_find_second_largest_element_in_array.js
@@ -1,14 +1,14 @@
-// Write a program in C to find the second largest element in an array.
-// Test Data:
-// Input the size of array: 5
-// Input 5 elements in the array:
-// element - 0 : 2
-// element - 1 : 9
-// element - 2 : 1
-// element - 3 : 4
-// element - 4 : 6
-// Expected Output:
-// The Second largest element in the array is: 6
+/* Write a program in C to find the second largest element in an array.
+Test Data:
+Input the size of array: 5
+Input 5 elements in the array:
+element - 0 : 2
+element - 1 : 9
+element - 2 : 1
+element - 3 : 4
+element - 4 : 6
+Expected Output:
+The Second largest element in the array is: 6 */
 
 
 const readArray = () => {
@@ -22,17 +22,24 @@ const readArray = () => {
     return array
 }
 
-const findSecondLargestElementInArray = (array) => {
-    let largestElement = array[0]
 
-    for (let i = 1; i < array.length; i++) {
+const findSecondLargestElementInArray = () => {
+    let largestElement = array[0];
+    let secondLargestElement = array[1];
+
+    for (let i = 2; i < array.length; i++) {
         if (array[i] > largestElement) {
-            largestElement = array[i]
+            secondLargestElement = largestElement;
+            largestElement = array[i];
+        } else if (array[i] > secondLargestElement && array[i] !== largestElement) {
+            secondLargestElement = array[i];
         }
-        return largestElement
     }
+    console.log(`The Second largest element in the array is: ${secondLargestElement}`);
 }
-
 
 const array = readArray()
 findSecondLargestElementInArray(array)
+
+
+

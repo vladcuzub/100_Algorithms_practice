b96ffc3b7d9d9922bc8fb454e280834ff4ae9da9
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-02 21:14:35 +0100

    add simple test for P_23 find the sum of right diagonals of a matrix

---
+++ src/C05_Array/P_23_find_the_sum_of_diagonal_right_matrix.js
@@ -0,0 +1,33 @@
+/*Write a program in C to find the sum of the right diagonals of a matrix.
+Test Data :
+Input the size of the square matrix : 2
+Input elements in the first matrix :
+element - [0],[0] : 1
+element - [0],[1] : 2
+element - [1],[0] : 3
+element - [1],[1] : 4
+Expected Output :
+The matrix is :
+1 2
+3 4
+Addition of the right Diagonal elements is :5 */
+
+
+
+const readInputMatrix = (sizeMatrix, matrixElements) => {
+    let matrix = [];
+
+    for (let i = 0; i < sizeMatrix; i++) {
+        let row = [];
+        for (let j = 0; j < sizeMatrix; j++) {
+            row[j] = matrixElements[i][j];
+        }
+        matrix[i] = row;
+    }
+    return matrix;
+}
+
+
+
+
+

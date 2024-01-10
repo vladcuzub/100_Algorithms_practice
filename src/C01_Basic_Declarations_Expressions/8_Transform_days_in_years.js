42a7d19b254119dc32864b35aa372422cf3db6d0
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-10 11:53:49 +0100

    delete some comments

--- src/C01_Basic_Declarations_Expressions/8_Transform_days_in_years.js
+++ src/C01_Basic_Declarations_Expressions/8_Transform_days_in_years.js
@@ -2,30 +2,6 @@
 // Note : Ignore leap year.
 // Test Data : Number of days : 1329 Expected Output : Years : 3 Weeks : 33 Days : 3
 
-// #include <stdio.h>
-
-
-// function convertDays(inputDays) {
-//   let daysInYear = 365;
-//   let daysInWeek = 7;
-
-//   let years = inputDays / daysInYear
-//   let remainingDays = inputDays % daysInYear;
-//   let weeks = remainingDays / daysInWeek
-
-//   let restDays = remainingDays % daysInWeek;
-//   console.log("Years: " + years + " Weeks: " + weeks + " Days: " + restDays);
-
-//   return {
-//     years: years,
-//       weeks: weeks,
-//         days: restDays
-//   };
-// }
-
-// console.log(convertDays(1329));
-
-
 let daysInYear = 365;
 let daysInWeek = 7
 

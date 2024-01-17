650a24a9b21961d13fed0d89928dfe97001a3c93
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-17 19:47:52 +0100

    add test and update the problem sum of 10 numbers

---
+++ spec/c03arrays/calculate/compute_10_numbersSpec.js
@@ -0,0 +1,11 @@
+describe('Compute the sum of the first 10 natural numbers', function () {
+
+    it('add each number together from 1 to 10', function () {
+
+        let result = calculate10naturlaNumbers()
+
+        expect(result).toBe(55)
+    })
+
+
+})
---
+++ src/c03arrays/calculate/compute_10_numbers.js
@@ -0,0 +1,15 @@
+// Write a program to compute the sum of the first 10 natural numbers.
+// Expected Output :
+// The first 10 natural number is :
+// 1 2 3 4 5 6 7 8 9 10
+// The Sum is : 55
+
+
+function calculate10naturlaNumbers() {
+  let sum = 0;
+
+  for (let i = 1; i <= 10; i++) {
+    sum += i;
+  }
+  return sum
+}

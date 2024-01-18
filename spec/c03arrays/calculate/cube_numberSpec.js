9848204ad618ba7df1eb97ea6c2d8a91cf4e4f6d
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-18 18:58:09 +0100

    test names updated

--- spec/c03arrays/calculate/cube_numberSpec.js
+++ spec/c03arrays/calculate/cube_numberSpec.js
@@ -1,6 +1,6 @@
 describe('Display the cube of the number up to an integer', function () {
 
-    it('the cube should be the multiplication three time of number', function () {
+    it('the cube should be the multiplication three time of 1', function () {
         let inputNumbers = 1
 
         let result = cubeOfNumber(inputNumbers)
@@ -8,7 +8,7 @@ describe('Display the cube of the number up to an integer', function () {
         expect(result).toBe(1)
     })
 
-    it('the cube should be the multiplication three time of number', function () {
+    it('the cube should be the multiplication three time of 2', function () {
         let inputNumbers = 2
 
         let result = cubeOfNumber(inputNumbers)
@@ -16,7 +16,7 @@ describe('Display the cube of the number up to an integer', function () {
         expect(result).toBe(8)
     })
 
-    it('the cube should be the multiplication three time of number', function () {
+    it('the cube should be the multiplication three time of 3', function () {
         let inputNumbers = 3
 
         let result = cubeOfNumber(inputNumbers)
@@ -24,7 +24,7 @@ describe('Display the cube of the number up to an integer', function () {
         expect(result).toBe(27)
     })
 
-    it('the cube should be the multiplication three time of number', function () {
+    it('the cube should be the multiplication three time of 4', function () {
         let inputNumbers = 4
 
         let result = cubeOfNumber(inputNumbers)
@@ -32,7 +32,7 @@ describe('Display the cube of the number up to an integer', function () {
         expect(result).toBe(64)
     })
 
-    it('the cube should be the multiplication three time of number', function () {
+    it('the cube should be the multiplication three time of 5', function () {
         let inputNumbers = 5
 
         let result = cubeOfNumber(inputNumbers)

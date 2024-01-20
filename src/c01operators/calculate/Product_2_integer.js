8eb1867fb1892ceb0ce2090a9cb618a31312c988
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-20 22:57:33 +0100

    format code

--- src/c01operators/calculate/Product_2_integer.js
+++ src/c01operators/calculate/Product_2_integer.js
@@ -1,6 +1,7 @@
 // Write a program that accepts two integers from the user and calculates the product of the two integers.Test Data : Input the first integer : 25 Input the second integer : 15 Expected Output : Product of the above two integers = 375
 
 
+
 function productOfNumbers(firstNumber,secondNumber) {
   let product = firstNumber*secondNumber;
 

b51996ccd4c670aa55be28e0bfa38f13f7940046
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-30 20:38:32 +0100

    change the name of test

--- spec/c02controlflow/search/find_heighest_factorySpec.js
+++ spec/c02controlflow/search/find_heighest_factorySpec.js
@@ -1,7 +1,7 @@
 const highestCommonFactor = require('../../../src/c02controlflow/search/find_heighest_factory')
 describe('find the HCF (Highest Common Factor) of two numbers.', function () {
 
-    it('Should find the smallest number from input numbers the numbers from 100 to 200 and find which are divisible to 9', function () {
+    it('Should find the HCF of 24 and 28, which is 4', function () {
         let firstNumber = 24
         let secondNumber = 28
 

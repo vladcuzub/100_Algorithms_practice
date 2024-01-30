4eb28cb2c9da2d05d587615f9c3562743c3c060a
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-01-30 20:23:29 +0100

    add test and update program function

--- src/c02controlflow/search/find_heighest_factory.js
+++ src/c02controlflow/search/find_heighest_factory.js
@@ -1,19 +1,21 @@
-// Write a C program to find the HCF (Highest Common Factor) of two numbers.
+// Write a program to find the HCF (Highest Common Factor) of two numbers.
 // Test Data :
 // Input 1st number for HCF: 24
 // Input 2nd number for HCF: 28
 // Expected Output :
 // HCF of 24 and 28 is : 4
 
-const highestCommonFactor = (firstNumber, secondNumber) => {
+
+function highestCommonFactor(firstNumber, secondNumber){
   let hcf;
   let smallNumber = firstNumber < secondNumber ? firstNumber : secondNumber;
 
-  for (let i = 1; i <= secondNumber; i++) {
+  for (let i = 1; i <= smallNumber; i++) {
     if (firstNumber % i === 0 && secondNumber % i === 0) {
       hcf = i
     }
   }
-  console.log(hcf)
+  return hcf
 }
-highestCommonFactor(24, 28)
+
+module.exports = highestCommonFactor

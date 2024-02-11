1eb2fb1548c6d17387f86e3f03d7df48c082b767
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-11 23:45:43 +0100

    add test and update problem find_sum_of_diagonal_right_matrix

---
+++ spec/c04matrices/calculate/find_the_sum_of_diagonal_right_matrixSpec.js
@@ -0,0 +1,13 @@
+const sumRightDiagonals = require('../../../src/c04matrices/calculate/find_the_sum_of_diagonal_right_matrix')
+
+describe('Sum of the right diagonals of a matrix', function () {
+
+    it('Should find the right diagonals of matrix and calculate the sum', function () {
+        let matrix = [[1,2],[3,4]]
+
+        let result = sumRightDiagonals(matrix)
+
+        expect(result).toEqual(5)
+    })
+
+})

d80a9646e0ff503ee80f2c9b7ec28d2f2c68f346
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-12 12:33:25 +0100

    add test and problem find_sum_rows_columns_of_matrix

---
+++ spec/c04matrices/search/find_sum_rows_columns_ofMatrixspec.js
@@ -0,0 +1,25 @@
+const sumRowsColumnsMatrix = require('../../../src/c04matrices/search/find_sum_rows_columns_ofMatrix')
+
+describe('Sum of rows and columns', function () {
+
+    it('Should calculate the sum of rows', function () {
+        let matrix = [[5, 6], [7, 8]]
+
+        let result = sumRowsColumnsMatrix.sumRowsMatrix(matrix)
+
+        let sumRows = [11,15]
+
+        expect(result).toEqual(sumRows)
+    })
+
+    it('Should calculate the sum of columns', function () {
+        let matrix = [[5, 6], [7, 8]]
+
+        let result = sumRowsColumnsMatrix.sumColumnssMatrix(matrix)
+
+        let sumColumns = [12, 14]
+
+        expect(result).toEqual(sumColumns)
+    })
+
+})

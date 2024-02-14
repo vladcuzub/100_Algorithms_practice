2499bb1766150cc4e4a7b37195fdb5ea86f39e84
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-14 20:52:37 +0100

    format matrix for test display_lowerTriangle_ofMatrix

--- spec/c04matrices/generate/display_lowerTriangle_ofMatrixSpec.js
+++ spec/c04matrices/generate/display_lowerTriangle_ofMatrixSpec.js
@@ -3,11 +3,15 @@ const lowerTriangleOfMatrix = require('../../../src/c04matrices/generate/display
 describe('Lower Triangle of Matrix', function () {
 
     it('Should print a lower triangle of matrix ', function () {
-        let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        let matrix = [[1, 2, 3],
+                      [4, 5, 6], 
+                      [7, 8, 9]]
 
         let result = lowerTriangleOfMatrix(matrix)
 
-        let lowerTriangle = [[1, 2, 3], [0, 5, 6], [0, 0, 9]]
+        let lowerTriangle = [[1, 2, 3],
+                             [0, 5, 6],
+                             [0, 0, 9]]
 
         expect(result).toEqual(lowerTriangle)
     })

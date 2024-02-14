b38b73b661aa121c93829c3998847ea11769d134
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-14 20:57:17 +0100

    remove else statement for problem display_lowerTriangle

--- src/c04matrices/generate/display_lowerTriangle_ofMatrix.js
+++ src/c04matrices/generate/display_lowerTriangle_ofMatrix.js
@@ -27,9 +27,7 @@ function lowerTriangleOfMatrix(matrix) {
     for (let i = 0; i < matrix.length; i++) {
 
         for (let j = 0; j < matrix.length; j++) {
-            if (i <= j) {
-                matrix[i][j]
-            } else {
+            if (i > j) {
                 matrix[i][j] = 0
             }
         }

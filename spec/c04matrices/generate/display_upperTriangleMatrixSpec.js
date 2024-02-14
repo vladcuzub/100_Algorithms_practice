ade5dca22c17f312cf208608d05075c17e0dd30c
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-14 20:53:08 +0100

    format matrix for test display_upperTriangleMatrix

--- spec/c04matrices/generate/display_upperTriangleMatrixSpec.js
+++ spec/c04matrices/generate/display_upperTriangleMatrixSpec.js
@@ -3,11 +3,15 @@ const upperTriangleMatrix = require('../../../src/c04matrices/generate/display_u
 describe('Upper Triangle of Matrix', function () {
 
     it('Should print a upper triangle of matrix ', function () {
-        let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        let matrix = [[1, 2, 3],
+                      [4, 5, 6], 
+                      [7, 8, 9]]
 
         let result = upperTriangleMatrix(matrix)
 
-        let upperTriangleResult = [[1, 0, 0], [4, 5, 0], [7, 8, 9]]
+        let upperTriangleResult = [[1, 0, 0],
+                                   [4, 5, 0],
+                                   [7, 8, 9]]
 
         expect(result).toEqual(upperTriangleResult)
     })

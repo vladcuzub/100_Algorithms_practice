aa0cb39b952f99a12c76b1f983295d91abe4a387
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-20 23:54:38 +0100

    small changes for logic function

--- src/c04matrices/generate/matrix_spiral_form.js
+++ src/c04matrices/generate/matrix_spiral_form.js
@@ -8,6 +8,10 @@ The given array in matrix form is:
 The spiral form of above matrix is:
 1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12 */
 
+let matrix = [[1, 2, 3, 4, 5],
+[6, 7, 8, 9, 10],
+[11, 12, 13, 14, 15],
+[16, 17, 18, 19, 20]]
 
 function matrixInSpiralForm (matrix) {
 
@@ -22,15 +26,23 @@ function matrixInSpiralForm (matrix) {
 
             for(let i = left; i < right; i++){
                 spiral[i] = matrix[top][i]
+                console.log(matrix[top][i])
             }
             top++
             for( let i = top;i < bottom; i++){
+                console.log(matrix[i][right-1])
                 spiral[i] = matrix[i][right - 1]
             }
             right++
+        for (let i = bottom; i < top; i++) {
+            console.log(matrix[i][right - 1])
+            spiral[i] = matrix[i][right - 1]
+        }
+        right++
 
     }
     return spiral
 }
 
+console.log(matrixInSpiralForm(matrix))
 module.exports = matrixInSpiralForm

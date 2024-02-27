d6f6f63bde4008f3917539a135cca740aa55c71d
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-27 20:06:17 +0100

    Update selection_sorting_algorithm.js

--- src/c07sorting/selection_sorting_algorithm.js
+++ src/c07sorting/selection_sorting_algorithm.js
@@ -7,16 +7,19 @@ b) To swap two elements */
 
 function sortElements(array) {
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
+        let minIndex = i;
         for (let j = i + 1; j < array.length; j++) {
-            if (array[j] < array[i]) {
-                let temp = array[j]
-                array[j] = array[i]
-                array[i] = temp
+            if (array[j] < array[minIndex]) {
+                minIndex = j
             }
         }
+        if (minIndex !== i) {
+            let temp = array[i]
+            array[i] = array[minIndex]
+            array[minIndex] = temp
+        }
     }
-
     return array
 }
 

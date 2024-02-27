a06afbc83969d664dc1294f11f90f1fad69093ae
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-02-27 19:39:39 +0100

    add test and problem selection_sorting_algorithm

--- src/c07sorting/selection_sorting_algorithm.js
+++ src/c07sorting/selection_sorting_algorithm.js
@@ -3,3 +3,22 @@ According to Wikipedia "In computer science, selection sort is a sorting algorit
     Note :
 a) To find maximum of elements
 b) To swap two elements */
+
+
+function sortElements(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[i]) {
+                let temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+            }
+        }
+    }
+
+    return array
+}
+
+
+module.exports = sortElements

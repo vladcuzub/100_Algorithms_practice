11be9fa2972a840dbdb093614c38a0776f699e45
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-03-04 12:25:39 +0100

    delete a useless variable

--- src/c07sorting/quick_sort.js
+++ src/c07sorting/quick_sort.js
@@ -2,8 +2,6 @@
 Quick sort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
 Note: Read n values into array and Sort using Quick Sort*/
 
-let array = [9, 6, 1, 20]
-
 function quickSort(array) {
     if (array.length <= 1) {
         return array;

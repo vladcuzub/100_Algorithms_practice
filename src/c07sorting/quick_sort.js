309e37f8b796c522332b786a1c8ac24f450975cf
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2024-03-04 12:17:59 +0100

    finished problem quick_sort_algorithm

--- src/c07sorting/quick_sort.js
+++ src/c07sorting/quick_sort.js
@@ -2,16 +2,43 @@
 Quick sort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined.
 Note: Read n values into array and Sort using Quick Sort*/
 
+let array = [9, 6, 1, 20]
+
 function quickSort(array) {
     if (array.length <= 1) {
-        return array
+        return array;
     }
 
-    const pivot = array(array.length - 1)
+    const pivot = array[array.length - 1]
+    let leftArray = []
+    let rightArray = []
 
+    for (let i = 0; i < array.length - 1; i++) {
 
-    return array
+        if (array[i] < pivot) {
+            leftArray[leftArray.length] = array[i]
+
+        } else {
+            rightArray[rightArray.length] = array[i]
+        }
+    }
+
+    const sortedLeft = quickSort(leftArray);
+    const sortedRight = quickSort(rightArray);
+    let index = 0;
+    const sortedArray = [];
+
+    sortedArray.length = sortedLeft.length + 1 + sortedRight.length;
+
+    for (let i = 0; i < sortedLeft.length; i++, index++) {
+        sortedArray[index] = sortedLeft[i];
+    }
+    sortedArray[index++] = pivot;
+    for (let i = 0; i < sortedRight.length; i++, index++) {
+        sortedArray[index] = sortedRight[i];
+    }
+
+    return sortedArray;
 }
-
 
 module.exports = quickSort

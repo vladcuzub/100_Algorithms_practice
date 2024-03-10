319ba3a2e3955872763b60172376dca4ec72e8ad
vladcuzub <vladalex1989@gmail.com>
2024-03-10 16:43:34 +0100

    added more test for convert_decimal_to_hexadecimal

--- spec/c02controlflow/generate/convert_decimal_to_hexadecimalSpec.js
+++ spec/c02controlflow/generate/convert_decimal_to_hexadecimalSpec.js
@@ -1,12 +1,22 @@
 const convertDecimalToHexadecimal = require('../../../src/c02controlflow/generate/convert_decimal_to_hexadecimal')
 
 describe('Convert decimal to hexadecimal', function () {
-    it('Shoult convert decimal number to hexadecimal number ', function () {
+    it('Should convert decimal number 79 to hexadecimal number wich is 4f ', function () {
 
         let decimalNumber = 79
 
         let result = convertDecimalToHexadecimal(decimalNumber)
 
         expect(result).toBe('4f')
+    })
+
+
+    it('Should convert decimal number 10 to hexadecimal number wich is a ', function () {
+
+        let decimalNumber = 10
+
+        let result = convertDecimalToHexadecimal(decimalNumber)
+
+        expect(result).toBe('a')
     })
 })

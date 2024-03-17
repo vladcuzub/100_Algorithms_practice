8713bbc4d5ea6e25b323684b82d165e6a2cbeb4a
vladcuzub <vladalex1989@gmail.com>
2024-03-17 13:16:38 +0100

    add test and problem mine_field

--- src/c08challenges/mine_field.js
+++ src/c08challenges/mine_field.js
@@ -16,4 +16,39 @@ Example output (for the above input)
 *211
 12*1
 0111
-*/ 
+*/
+
+
+function mineFeld(input) {
+  let output = []
+  let row = input.length
+  let col = input[0].length
+
+  for (let i = 0; i < row; i++) {
+    output[i] = [];
+
+    for (let j = 0; j < col; j++) {
+      output[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+
+      if (input[i][j] === '*') {
+        output[i][j] = '*'
+      } else {
+        for (let x = i - 1; x <= i + 1; x++) {
+          for (let y = j - 1; y <= j + 1; y++) {
+            if (x >= 0 && x < row && y >= 0 && y < col && input[x][y] === '*') {
+              output[i][j]++;
+            }
+          }
+        }
+      }
+    }
+  }
+  return output
+}
+
+module.exports = mineFeld

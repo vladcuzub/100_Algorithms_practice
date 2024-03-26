8af966e7f2f8bd049e1c25d4cb13b827a0838a5d
vladcuzub <vladalex1989@gmail.com>
2024-03-26 22:04:11 +0100

    add more tests cases for chuncky_monkey

--- spec/c08chalenges/chunck_monkeySpec.js
+++ spec/c08chalenges/chunck_monkeySpec.js
@@ -10,4 +10,24 @@ describe('Chuncky monkey', function () {
 
         expect(result).toEqual([["a", "b"], ["c", "d"]])
     })
+
+    it('Should split an array into groups the length of size and return them is two dimensional array', function () {
+
+        let arr = [0, 1, 2, 3, 4, 5]
+        let size = 3
+
+        let result = chunkyArrayInGroups(arr, size)
+
+        expect(result).toEqual([[0, 1, 2], [3, 4, 5]])
+    })
+
+    it('Should split an array into groups the length of size and return them is two dimensional array', function () {
+
+        let arr = [0, 1, 2, 3, 4, 5, 6]
+        let size = 3
+
+        let result = chunkyArrayInGroups(arr, size)
+
+        expect(result).toEqual([[0, 1, 2], [3, 4, 5], [6]])
+    })
 })

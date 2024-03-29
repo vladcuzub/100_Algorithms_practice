c509e80188ae5a13cc87303c07862d66d1670b56
vladcuzub <vladalex1989@gmail.com>
2024-03-29 23:10:15 +0100

    remove methods for problem non_overlapping_interval

--- src/c08challenges/non_overlapping_interval.js
+++ src/c08challenges/non_overlapping_interval.js
@@ -4,33 +4,34 @@ Input: Set : [1, 3], [6, 9]
 New Interval : [2, 5]
 Output: [1, 5], [6, 9]
 */
-
 function insertInterval(array, newInterval) {
     let intervalResult = []
     let i = 0;
 
-    // Add intervals that come before the new interval
     while (i < array.length && array[i][1] < newInterval[0]) {
         intervalResult[intervalResult.length] = array[i];
         i++;
     }
 
-    // Merge overlapping intervals
     while (i < array.length && array[i][0] <= newInterval[1]) {
-        newInterval[0] = Math.min(newInterval[0], array[i][0]);
-        newInterval[1] = Math.max(newInterval[1], array[i][1]);
+        if (array[i][0] < newInterval[0]) {
+            newInterval[0] = array[i][0];
+        }
+        if (array[i][1] > newInterval[1]) {
+            newInterval[1] = array[i][1];
+        }
         i++;
     }
-    intervalResult.push(newInterval);
 
-    // Add intervals that come after the new interval
+    intervalResult[intervalResult.length] = newInterval;
+
     while (i < array.length) {
-        intervalResult.push(array[i]);
+        intervalResult[intervalResult.length] = array[i];
         i++;
     }
 
     return intervalResult;
 }
-console.log(insertInterval([[1, 3], [6, 9]], [2, 5]))
+
 
 module.exports = insertInterval

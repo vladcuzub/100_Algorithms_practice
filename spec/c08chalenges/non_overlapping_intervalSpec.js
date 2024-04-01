9efe7f55e5de516297f97eed370257eb5f925018
vladcuzub <vladalex1989@gmail.com>
2024-04-01 11:31:00 +0200

    rename tests for non_overlapping_interval

--- spec/c08chalenges/non_overlapping_intervalSpec.js
+++ spec/c08chalenges/non_overlapping_intervalSpec.js
@@ -1,7 +1,7 @@
 const insertInterval = require('../../src/c08challenges/non_overlapping_interval')
 
 describe('No overlapping intervals', function () {
-    it('Should find the correct position of non-overlaping intervals', function () {
+    it('Should insert the interval in the correct position', function () {
 
         let array = [[1, 3], [6, 9]]
         let newInterval = [2, 5]
@@ -10,7 +10,7 @@ describe('No overlapping intervals', function () {
 
         expect(result).toEqual([[1, 5], [6, 9]])
     })
-    it('Should find the correct position of non-overlaping intervals', function () {
+    it('Should insert the interval in the correct position', function () {
 
         let array = [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]]
         let newInterval = [4, 9]
